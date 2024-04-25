897e6447137364691b9dafb6accd97e861b2c5c4
neritsuko <alina22ibraimova@gmail.com>
2024-04-25 21:12:09 +0500

    fourth commit

---
+++ project/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  project
+//
+//  Created by alina ibraimova on 25.04.2024.
+//
+
+import Foundation
---
+++ project/UserManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserManager.swift
+//  project
+//
+//  Created by alina ibraimova on 25.04.2024.
+//
+
+import Foundation
